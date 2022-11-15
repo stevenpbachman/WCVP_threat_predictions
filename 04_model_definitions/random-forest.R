b6486468b644c645a7fb7eb676ba657c28bbd7bb
barnabywalker <barnaby.walker08@gmail.com>
2022-11-15 16:43:21 +0000

    updated rf predictors to include human footprint

--- 04_model_definitions/random-forest.R
+++ 04_model_definitions/random-forest.R
@@ -14,49 +14,50 @@ specify_recipe <- function(data, ...) {
   outcome <- "obs"
   predictors <- c(
     "L3_count",
-    "humphreys_lifeform",
-    "climate_description",
-    "area"
+    "humphreys_lifeform"
   )
   impute_phylo <- c(
     "higher_groups",
     "order",
     "family",
     "genus",
-    "climate_description",
     "humphreys_lifeform"
   )
 
   rec <- 
     data |>
-    select(all_of(c(outcome, predictors, impute_phylo)), starts_with("pvr"), starts_with("biome")) |>
+    select(all_of(c(outcome, predictors, impute_phylo)), matches("pvr([0-9]|[0-4][0-9]|[50])$"), starts_with("biome"), starts_with("hfp")) |>
     recipe() |>
     update_role(all_of(outcome), new_role="outcome") |>
     update_role(one_of(predictors), new_role="predictor") |>
     update_role(starts_with("pvr"), new_role="predictor") |>
     update_role(starts_with("biome"), new_role="predictor") |>
+    update_role(starts_with("hfp"), new_role="predictor") |>
     update_role(setdiff(impute_phylo, predictors), new_role="impute phylo") |>
     add_role(intersect(predictors, impute_phylo), new_role="impute phylo") |>
     step_unknown(
-      humphreys_lifeform,
-      climate_description
+      humphreys_lifeform
     ) |>
     step_impute_knn(
       starts_with("pvr"), 
       impute_with=imp_vars(has_role(match="impute phylo"))
     ) |>
-    step_impute_bag(
+    step_impute_knn(
       starts_with("biome"),
       impute_with=imp_vars(starts_with("pvr"))
     ) |>
-    step_log(L3_count, area)
+    step_impute_knn(
+      starts_with("hfp"),
+      impute_with=imp_vars(starts_with("biome"))
+    ) |>
+    step_log(L3_count)
 }
 
 # HYPERPARAMETERS ----
 make_grid <- function(data) {
   grid_regular(
     min_n(),
-    finalize(mtry(), data),
+    mtry(range=c(3, 15)),
     levels=3
   )
 }
